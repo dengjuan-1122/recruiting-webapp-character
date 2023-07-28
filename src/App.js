e55ae8ae93b18801bb444671c92ff90684a5c9a3
juandeng <dengjuan991122@gmail.com>
2023-07-28 10:06:38 -0400

    Add step 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
 
 function App() {
   const [counters, setCounters] = React.useState([0, 0, 0, 0, 0, 0]);
+
+  const changeColor = (counters, s, d, c, i, w, h) => {
+    if (counters[0] >= s && counters[1] >= d && counters[2] >= c && counters[3] >= i && counters[4] >= w && counters[5] >= h) {
+      return 'red';
+    }
+    return '';
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -37,6 +44,24 @@ function App() {
 
             </div>
          ))}
+        </div>
+        <div>
+          Classes
+          {Object.entries(CLASS_LIST).map(([key, value]) =>
+            <div key={key}>
+              <button style={{
+                color: changeColor(counters,
+                  value['Strength'],
+                  value['Dexterity'],
+                  value['Constitution'],
+                  value['Intelligence'],
+                  value['Wisdom'],
+                  value['Charisma'])
+              }}>
+                {key}
+              </button>
+            </div>
+          )}
         </div>
       </section>
     </div>
