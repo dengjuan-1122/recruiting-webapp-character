065159202783195778f8688023e0a69bf45da0e1
juandeng <dengjuan991122@gmail.com>
2023-07-28 09:31:29 -0400

    Add step 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
 
 
 function App() {
-  const [num, setNum] = useState(0);
+  const [counters, setCounters] = React.useState([0, 0, 0, 0, 0, 0]);
   return (
     <div className="App">
       <header className="App-header">
@@ -12,10 +12,31 @@ function App() {
       </header>
       <section className="App-section">
         <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
+          Attributes
+          {counters.map((value, index) => (
+            <div key={ATTRIBUTE_LIST[index]}>
+              <p>
+                {ATTRIBUTE_LIST[index]}: {value}
+                <button
+                  onClick={() => {
+                    const countersCopy = [...counters];
+                    countersCopy[index] += 1;
+                    setCounters(countersCopy);
+                  }}>
+                  +
+                </button>
+                <button
+                  onClick={() => {
+                    const countersCopy = [...counters];
+                    countersCopy[index] -= 1;
+                    setCounters(countersCopy);
+                  }}>
+                  -
+                </button>
+              </p>
+
+            </div>
+         ))}
         </div>
       </section>
     </div>
